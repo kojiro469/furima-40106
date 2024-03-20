f4f01ace6aaef5f06ac000ac72f29fd3b4bdb895
kojiro469 <kohjiroh469@yahoo.co.jp>
2024-03-20 19:04:33 +0900

    Formオブジェクトの設定

---
+++ app/models/order_shipping.rb
@@ -0,0 +1,19 @@
+class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "ハイフンを含めないでください"}
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
